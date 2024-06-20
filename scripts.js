6e29f462ae99efe07a931acaf0e4b994302daad3
davi <161468196+da6zin@users.noreply.github.com>
2024-06-20 18:52:23 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,21 @@
+document.getElementById('menu-button').addEventListener('click', function() {
+    document.getElementById('sidebar').style.width = '250px';
+});
+
+document.getElementById('close-sidebar').addEventListener('click', function() {
+    document.getElementById('sidebar').style.width = '0';
+});
+
+document.getElementById('home-button').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = 'index.html';  // Troque 'index.html' pelo caminho correto do seu arquivo
+});
+
+// Redirecionar para outras páginas ao clicar nos botões
+document.getElementById('pending-projects').addEventListener('click', function() {
+    window.location.href = 'projetos-pendentes.html';  // Troque pelo caminho correto do seu arquivo
+});
+
+document.getElementById('accepted-projects').addEventListener('click', function() {
+    window.location.href = 'projetos-aceitos.html';  // Troque pelo caminho correto do seu arquivo
+});
